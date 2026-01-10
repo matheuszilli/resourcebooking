23cf3cd8136d7e667faeb2dd7d24d1a29289697e
Matheus Zilli <matheuszilli@outlook.com>
2026-01-10 19:35:52 -0300

    Commit Inicial

---
+++ src/main/java/br/com/matheuszilli/resourcebooking/ResourcebookingApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheuszilli.resourcebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcebookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcebookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheuszilli/resourcebooking/ResourcebookingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheuszilli.resourcebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcebookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
