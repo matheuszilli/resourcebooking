7efd942353b68d8b280c4411102e03faddd85263
Matheus Zilli <matheus.zilli@al.infnet.edu.br>
2026-01-13 20:41:51 -0300

    Feat: Mudado getters and setters para @getter e @setter

--- src/main/java/br/com/matheuszilli/resourcebooking/booking/Booking.java
+++ src/main/java/br/com/matheuszilli/resourcebooking/booking/Booking.java
@@ -2,11 +2,16 @@ package br.com.matheuszilli.resourcebooking.booking;
 
 import br.com.matheuszilli.resourcebooking.resource.Resource;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+
 @Entity
 @Table(name = "tb_booking")
+@Getter
+@Setter
 public class Booking {
 
     @Id
@@ -39,61 +44,4 @@ public class Booking {
         this.reason = reason;
     }
 
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Resource getResource() {
-        return resource;
-    }
-
-    public void setResource(Resource resource) {
-        this.resource = resource;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public LocalDateTime getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(LocalDateTime startTime) {
-        this.startTime = startTime;
-    }
-
-    public LocalDateTime getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(LocalDateTime endTime) {
-        this.endTime = endTime;
-    }
-
-    public BookingStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(BookingStatus status) {
-        this.status = status;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
 }
--- src/main/java/br/com/matheuszilli/resourcebooking/resource/Resource.java
+++ src/main/java/br/com/matheuszilli/resourcebooking/resource/Resource.java
@@ -1,8 +1,12 @@
 package br.com.matheuszilli.resourcebooking.resource;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "tb_resource")
 public class Resource {
 
@@ -27,49 +31,6 @@ public class Resource {
         this.name = name;
         this.type = type;
         this.capacity = capacity;
-        this.active = active;
-    }
-
-    // Getters and Setters
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ResourceType getType() {
-        return type;
-    }
-
-    public void setType(ResourceType type) {
-        this.type = type;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
         this.active = active;
     }
 }
